50acd733eed6561318af4157c07db65b50478f65
Auris <aurimas.lickus@spryker.com>
2016-10-19 11:18:25 +0200

    Move tax logic to tax product connector

---
+++ src/Spryker/Zed/TaxProductConnector/Communication/Plugin/TaxSetProductAbstractReadPlugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\TaxProductConnector\Communication\Plugin;
+
+use Generated\Shared\Transfer\ProductAbstractTransfer;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\Product\Dependency\Plugin\ProductAbstractPluginInterface;
+
+/**
+ * @method \Spryker\Zed\TaxProductConnector\Business\TaxProductConnectorFacade getFacade()
+ */
+class TaxSetProductAbstractReadPlugin extends AbstractPlugin implements ProductAbstractPluginInterface
+{
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductAbstractTransfer $productAbstractTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductAbstractTransfer
+     */
+    public function run(ProductAbstractTransfer $productAbstractTransfer)
+    {
+         return $this->getFacade()
+             ->addTaxSet($productAbstractTransfer);
+    }
+
+}
